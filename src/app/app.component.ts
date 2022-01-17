9a7910745b312a14d05385c5bfa31eec082397ef
Hicham Abdelwahab <you@example.com>
2022-01-17 21:48:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent 
+{
   title = 'Kanban-fire';
+  todo: Task[] = [
+
+    { 
+      title: 'buy milk',
+      description: 'Using Fire base and Angular create a kanban app!'
+    }
+  ]
 }
